145a88aa87415b2d57f19c915488d156d2827826
quillcraftsman <quill@craftsman.lol>
2023-08-18 21:45:25 +0400

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,21 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
+def read(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
 setup(
     name='find-similar',
     version='1.0',
     packages=['algorithm'],
     include_package_data=True,
+    license='MIT',
     description='Algorithm to define similarity rating between objects',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/findsimilar/find-similar',
     author='findsimilar',
     author_email='help@findsimilar.org',
@@ -24,5 +33,6 @@ setup(
         'pydantic==1.10.7'
     ],
     python_requires='>=3',
-    classifiers=[],
 )
+
+
