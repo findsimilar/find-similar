adf41dc8ea5f5f9c8cf4b49b622e660923a13752
quillcraftsman <quill@craftsman.lol>
2023-08-21 20:20:49 +0400

    add simple usage

--- find_similar/__init__.py
+++ find_similar/__init__.py
@@ -1,6 +1,18 @@
 """
 User-friendly library to find similar objects.
 Main package.
+
+Install:
+
+pip install find-similar
+
+Usage:
+
+from find_similar import find_similar
+
+texts = ['one two', 'two three', 'three four'] \n
+result = find_similar('one four', texts, count=10)
+
 """
 from .core import find_similar
 from .calc_functions import calc_cosine_similarity_opt, get_tokens, TokenText
