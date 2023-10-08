099ac37331aaef082aa0705aaa96f3e97a7e9925
quillcraftsman <quill@craftsman.lol>
2023-10-08 18:23:57 +0400

    add tokenize one web page

--- laboratory/analysis/tests/tests_views.py
+++ laboratory/analysis/tests/tests_views.py
@@ -4,30 +4,43 @@ Tests form views
 from django.test import SimpleTestCase
 from analysis.forms import OneTextForm
 
+URL = '/analysis/tokenize-one/'
 
-class TestTokenizeOneView(SimpleTestCase):
+
+class TestTokenizeOneViewGet(SimpleTestCase):
     """
-    Test TokenizeOneView
+    Test TokenizeOneView GET
     """
 
     def setUp(self):
         """
         Setup test data
         """
-        url = '/analysis/tokenize-one/'
-        self.response = self.client.get(url)
+        # self.response = self.client.get(URL)
+        self.one = 'oneincdjm2283'
+        self.two = 'uekemee68'
+        self.text = f'{self.one} {self.two}'
+
+        def get_empty_params():
+            return self.client.get(URL)
+
+        def get_with_params():
+            return self.client.get(f'{URL}?text={self.text}&token={self.one}&token={self.two}')
+
+        self.response = get_empty_params
+        self.param_response = get_with_params
 
     def test_status_code(self):
         """
         check available
         """
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response().status_code, 200)
 
     def test_form_in_context(self):
         """
         check context
         """
-        context = self.response.context
+        context = self.response().context
         form = 'form'
         self.assertIn(form, context)
         self.assertTrue(isinstance(context[form], OneTextForm))
@@ -38,5 +51,62 @@ class TestTokenizeOneView(SimpleTestCase):
         """
         count = 1
         element = 'form'
-        self.assertContains(self.response, f'<{element} method="post">', count)
-        self.assertContains(self.response, f'</{element}>', count)
+        self.assertContains(self.response(), f'<{element} method="post">', count)
+        self.assertContains(self.response(), f'</{element}>', count)
+
+    def test_response_params(self):
+        """
+        Test response with params
+        And without
+        """
+        response = self.param_response()
+        self.assertContains(response, self.text, 1)
+        count = 2  # one in text
+        self.assertContains(response, self.one, count)
+        self.assertContains(response, self.two, count)
+        # without params
+        response = self.response()
+        self.assertNotContains(response, self.one)
+        self.assertNotContains(response, self.text)
+
+    def test_context_params(self):
+        """
+        Test response context with params
+        And without
+        """
+        # with params
+        tokens = 'tokens'
+        text = 'text'
+        context = self.param_response().context
+        # text
+        self.assertIn(text, context)
+        self.assertEqual(context[text], self.text)
+        # tokens
+        self.assertIn(tokens, context)
+        self.assertEqual(context[tokens], [self.one, self.two])
+        # without params
+        context = self.response().context
+        self.assertNotIn(context[tokens], [])
+        self.assertEqual(context[text], '')
+
+
+class TestTokenizeOneViewPost(SimpleTestCase):
+    """
+    Test TokenizeOneView POST
+    """
+
+    def setUp(self):
+        """
+        Setup test data
+        """
+        data = {
+            'text': 'one two'
+        }
+        self.response = self.client.post(URL, data=data)
+
+    def test_valid_redirect(self):
+        """
+        Test redirect with valid data
+        """
+        expected_url_params = '?text=one two&token=one&token=two'
+        self.assertRedirects(self.response, f'{URL}{expected_url_params}')
--- laboratory/analysis/views.py
+++ laboratory/analysis/views.py
@@ -1,8 +1,9 @@
 """
 Analysis views
 """
-from django.urls import reverse_lazy
 from django.views.generic import FormView
+from django.urls import reverse
+from analysis.functions import analyze_one_item
 from .forms import OneTextForm
 
 
@@ -12,4 +13,27 @@ class TokenizeOneView(FormView):
     """
     form_class = OneTextForm
     template_name = 'analysis/tokenize_one.html'
-    success_url = reverse_lazy('analysis:tokenize_one')
+
+    def form_valid(self, form):
+        text = form.cleaned_data['text']
+        self.text = text
+        self.tokens = analyze_one_item(text)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        data = self.request.GET
+        text = data.get('text', '')
+        tokens = data.getlist('token')
+        context['text'] = text
+        context['tokens'] = tokens
+        return context
+
+    def get_success_url(self):
+        url_params = []
+        for token in self.tokens:
+            param = f'token={token}'
+            url_params.append(param)
+        url_params = f'?text={self.text}&{"&".join(url_params)}'
+        url = f'{reverse("analysis:tokenize_one")}{url_params}'
+        return url
