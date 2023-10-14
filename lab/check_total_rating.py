72372cf523150455df1ff6faf2cb1e2ceaedda12
vtyushkevich <vatyushkevich@yandex.ru>
2023-10-14 23:26:07 +0500

    fix linter fail

--- lab/check_total_rating.py
+++ lab/check_total_rating.py
@@ -17,7 +17,6 @@ from database.db_functions import (  # pylint: disable=wrong-import-position
 )
 
 
-
 print("Load base items...")
 base_list = get_all_base_items()
 base_list_tokens = get_all_base_tokens(base_list, dictionary=DICTIONARY)
@@ -27,24 +26,31 @@ print("Load analog items...")
 analog_list_tokens = get_all_analog_tokens()
 print(f"{len(analog_list_tokens)} items loaded")
 
-print("CHECK RATING...")
-start_time = datetime.now()
-rating_total = check_ratings_total(base_list_tokens, analog_list_tokens, DICTIONARY)
-end_time = datetime.now()
-time_diff = end_time - start_time
-
 try:
-    _, is_save_file, *others = sys.argv
+    _, base_in_analog, is_save_file, *others = sys.argv
 except ValueError:
     is_save_file = False  # pylint: disable=invalid-name
     output = print
+    base_in_analog = False
 else:
     filename = str(datetime.now()).replace(":", "-")  # pylint: disable=invalid-name
+    print(filename)
     path = os.path.join("reports", filename)
     f = open(path, 'w', encoding='utf-8')  # pylint: disable=consider-using-with
     output = lambda s: f.write(f'{s}\n')  # pylint: disable=unnecessary-lambda-assignment
     # with open(path, "w", encoding="utf-8") as f:
     #     output = lambda s: f.write(f"{s}\n")  # pylint: disable=unnecessary-lambda-assignment
+
+print("CHECK RATING...")
+start_time = datetime.now()
+if base_in_analog:
+    print("FINDING BASE IN ANALOG...")
+    rating_total = check_ratings_total(analog_list_tokens, base_list_tokens, DICTIONARY)
+else:
+    print("FINDING ANALOG IN BASE...")
+    rating_total = check_ratings_total(base_list_tokens, analog_list_tokens, DICTIONARY)
+end_time = datetime.now()
+time_diff = end_time - start_time
 
 output("CREATE REPORT")
 get_report_total(rating_total, output=output)
