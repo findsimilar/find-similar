07938ce02ebcbe2b73ccfb08f30c07902dfffe6f
DanteOnline <iamdanteonline@gmail.com>
2023-11-10 16:01:43 +0400

    remove Item unuseful model

--- find_similar/calc_models.py
+++ find_similar/calc_models.py
@@ -1,24 +1,6 @@
 """
 Models for calculation and compare
 """
-from typing import Optional
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
-
-
-class Item(BaseModel):
-    """
-    Item
-    """
-    id: Optional[int]  # pylint: disable=invalid-name
-    label: str
-    part_number: str
-    id_shop: int
-    id_base_item: Optional[int]
-    cos: Optional[float] = 0
-    token_set: Optional[set] = {}
-
-    def __eq__(self, other):
-        return self.id == other.id_base_item
 
 
 class TokenizeException(Exception):
